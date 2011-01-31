4e8b27e82ca9144b68c59710d438db035744f275
Matias Valdenegro T <matias.valdenegro@gmail.com>
2011-01-31 16:33:58 -0300

    Avanzar aqui.

--- source/epic.core/Pointer.h
+++ source/epic.core/Pointer.h
@@ -20,32 +20,93 @@ namespace Epic {
         template<typename T>
         class Pointer {
             public:
-                Pointer();
-                Pointer(T *pointer);
-                Pointer(const Pointer<T>& orig);
-                ~Pointer();
+                Pointer() : pointer(nullptr)
+                {
+                }
 
-                T& operator*() const;
-                T *operator->() const;
+                Pointer(T *ptr) : pointer(ptr)
+                {
+                }
+
+                Pointer(Pointer<T>& orig) : pointer(nullptr)
+                {
+                    attach(orig);
+                }
+
+                ~Pointer()
+                {
+                    detach();
+                }
+
+                Pointer<T>& operator=(T *ptr)
+                {
+                    attach(ptr);
+                }
+
+                Pointer<T>& operator=(Pointer<T>& orig)
+                {
+                    attach(orig);
+                }
+
+                T& operator*()
+                {
+                    return *(this->pointer);
+                }
+                const T& operator*() const
+                {
+                    return *(this->pointer);
+                }
+
+                T* operator->()
+                {
+                    return this->pointer;
+                }
+
+                const T* operator->() const
+                {
+                    return this->pointer;
+                }
 
                 operator bool() const;
                 bool operator!() const;
 
+                bool isNull() const
+                {
+                    return this->pointer == nullptr;
+                }
+
+                bool isNotNull() const
+                {
+                    return this->pointer != nullptr;
+                }
+
+                void attach(T *ptr)
+                {
+                    detach();
+
+                    this->pointer = ptr;
+                }
+                void attach(Pointer<T>& orig)
+                {
+                    detach();
+
+                    this->pointer = orig.pointer;
+                    orig.pointer = nullptr;
+                }
+
+                void detach()
+                {
+                    if(this->pointer != nullptr) {
+                        delete pointer;
+
+                        pointer = nullptr;
+                    }
+                }
+
             private:
-                T *mPointer;
+                T *pointer;
 
         };
-
-        template<typename T>
-        Pointer<T>::Pointer() : mPointer(nullptr)
-        {
-        }
-
-        template<typename T>
-        Pointer<T>::Pointer(T *pointer) : mPointer(pointer)
-        {
-        }
-
     };
 };
 
