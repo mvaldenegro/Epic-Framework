ee0fe45ba948a4569d9a8c5e9afee54e68924cba
Matias Valdenegro T <matias.valdenegro@gmail.com>
2011-01-30 23:42:42 -0300

    Add map and memory utilities.

--- source/epic.core/Algorithms.h
+++ source/epic.core/Algorithms.h
@@ -8,6 +8,9 @@
 #ifndef ALGORITHMS_H
 #define	ALGORITHMS_H
 
+#include <epic.core/Core.h>
+#include <epic.core/Array.h>
+
 namespace Epic {
     namespace Core {
         template<typename T>
@@ -20,6 +23,34 @@ namespace Epic {
         T max(const T& a, const T& b)
         {
             return a < b ? b : a;
+        }
+
+        template<typename T>
+        void copyMemory(T *destiny, const T* source, size_t size)
+        {
+            for(size_t i = 0; i < size; i++) {
+                destiny[i] = source[i];
+            }
+        }
+
+        template<typename T>
+        void fillMemory(T *destiny, const T& constData, size_t size)
+        {
+            for(size_t i = 0; i < size; i++) {
+                destiny[i] = constData;
+            }
+        }
+
+        template<typename T, typename Function>
+        Array<T> map(const Array<T>& input, Function f)
+        {
+            Array<T> ret;
+
+            for(int i = 0; i < input.size(); i++) {
+                ret[i] f(input[i]);
+            }
+
+            return ret;
         }
     }
 }
